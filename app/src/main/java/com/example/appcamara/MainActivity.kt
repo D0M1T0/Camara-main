cbfe134707f43e7590f684e7d70215f0da98f5e7
D0M1T0 <florireyes150@gmail.com>
2024-03-11 13:04:44 -0500

    hello world

---
+++ app/src/main/java/com/example/appcamara/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.appcamara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.appcamara.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btncompartir.setOnClickListener(this)
+        binding.btntomarfoto.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btntomarfoto -> tomarfoto()
+            R.id.btncompartir -> compartirfoto()
+        }
+    }
+
+    private fun compartirfoto() {
+        TODO("Not yet implemented")
+    }
+
+    private fun tomarfoto() {
+        abrircamara.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+    }
+    private val abrircamara = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode== RESULT_OK){
+            val data = result.data!!
+            val imagenBitmap = data.extras!!.get("data") as Bitmap
+            binding.ivimage.setImageBitmap(imagenBitmap)
+        }
+    }
+}
